91a89403c95cd5bb3e25e8b4a5949f3d6b60b188
Adam Lamson <adam.lamson@colorado.edu>
2019-05-29 17:17:51 -0600

    Started moving over files from other directory.

---
+++ chi_pet/chi.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# Basic
+import sys
+import os
+import shutil
+import pdb
+import yaml
+import argparse
+# Analysis
+import re
+from chi_parser import chi_parser
+from chi_lib import *
+# from ChiParams import ChiParam, ChiSim
+# from ChiLaunch import ChiLaunch
+# from ChiCreate import ChiCreate
+# from ChiParticleSwarm import ChiParticleSwarm
+# from ChiGeneticAlgorithm import ChiGeneticAlgorithm
+# from ChiRun import ChiRun
+
+'''
+Name: Chi.py
+Description: Main control program for xi(chi)-launcher. Runs all newagebob code.
+Input: To view options type Chi.py -h
+Output:
+'''
+
+
+class Chi(object):
+    def __init__(self, opts):
+
+        self.opts = opts
+        self.yml_files_dict = {}  # combined dictionary of all yaml files
+        self.cwd = os.getcwd()
+        self.ChiParams = []
+        self.ReadOpts()
+        self.ProgOpts()
+
+    def ReadOpts(self):
+        # TODO This might not be fully integrated just yet
+        if not self.opts.workdir:
+            self.opts.workdir = self.cwd
+
+        if not self.opts.states and self.opts.args_file:
+            yd = CreateDictFromYamlFile(self.opts.args_file)
+            self.opts.states = list(yd.keys())
+
+    def ProgOpts(self):
+        wd = self.opts.workdir  # Shortcut for work directory
+        if self.opts.launch != "NOLAUNCH":
+            # If no sim dirs are given find them all in simulations
+            if self.opts.launch == []:
+                self.opts.launch = find_dirs(os.path.join(wd, "simulations"))
+                # If no dirs were found return with warning
+                if self.opts.launch == []:
+                    print(" No sim directories were found our given. ")
+                    return
+            # Find run.not and delete
+            try:
+                os.remove(os.path.join(wd, "run.not"))
+            except OSError:
+                print(
+                    "run.not was not found in workdir. Might want to go searching for it.")
+                pass
+            # Create run.ing in workdir
+            touch(os.path.join(wd, "run.ing"))
+            ChiLaunch(simdirs=self.opts.launch, opts=self.opts)
+
+        elif self.opts.create:
+            touch(os.path.join(wd, "run.not"))
+            c = ChiCreate(self.opts, self.opts.workdir)
+            c.Create(self.opts.create)
+
+        elif self.opts.shotgun:
+            c = ChiCreate(self.opts, self.opts.workdir)
+            c.Create(self.opts.shotgun)
+
+        elif self.opts.particleswarmcreate:
+            c = ChiParticleSwarm(self.opts, self.opts.workdir, 0)
+            c.Create(self.opts.particleswarmcreate)
+
+        elif self.opts.geneticalgorithmcreate:
+            c = ChiGeneticAlgorithm(self.opts, self.opts.workdir, 0)
+            c.Create(self.opts.geneticalgorithmcreate)
+
+        elif self.opts.run:
+            c = ChiRun(self.opts)
+            c.Run(self.opts)
+
+        elif self.opts.prep:
+            seed_lst = find_seed_dirs(self.opts.workdir)
+            for sd_dir in seed_lst:
+                if self.opts.args_file:
+                    shutil.copy(self.opts.args_file, sd_dir)
+                for s in self.opts.states:
+                    touch(os.path.join(sd_dir, 'sim.{}'.format(s)))
+
+        elif self.opts.remove:
+            seed_lst = find_seed_dirs(self.opts.workdir)
+            for sd_dir in seed_lst:
+                for fn in self.opts.remove:
+                    path = os.path.join(sd_dir, fn)
+                    if os.path.exists(path):
+                        os.remove(path)
+
+
+##########################################
+if __name__ == "__main__":
+    opts = chi_parser()
+    x = Chi(opts)
+    # x.MakeDirectoryStruct()
---
+++ chi_pet/chi_lib.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+import sys
+import os
+import pdb
+import re
+import shutil
+import yaml
+from collections import OrderedDict
+from math import *
+
+'''
+Name: ChiLib.py
+Description: Library of used functions for Chi-Launcher
+'''
+
+
+def CreateYamlFilesFromDict(seed_dir, yml_file_dict):
+    for f, d in yml_file_dict.items():
+        path = os.path.join(seed_dir, f)
+        with open(path, 'w') as of:
+            OrderedYamlDump(d, of, default_flow_style=False)
+
+
+def CreateDictFromYamlFile(path):
+    with open(path, 'r') as f:
+        ydict = OrderedYamlLoad(f)
+        return ydict
+
+
+def OrderedYamlLoad(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    ''' Taken from http://stackoverflow.com/questions/5121931/in-python-how-can-you-load-yaml-mappings-as-ordereddicts/21048064#21048064
+    Usage: yaml_dict = OrderedYamlLoad(istream)
+    '''
+   class OrderedLoader(Loader):
+        pass
+
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+
+
+
+def OrderedYamlDump(data, stream=None, Dumper=yaml.Dumper, **kwds):
+    ''' Taken from http://stackoverflow.com/questions/5121931/in-python-how-can-you-load-yaml-mappings-as-ordereddicts/21048064#21048064
+    Usage: OrderedYamlDump(yaml_dictionary, ostream)
+    '''
+    class OrderedDumper(Dumper):
+        pass
+
+    def _dict_representer(dumper, data):
+        return dumper.represent_mapping(
+            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+            list(data.items()))
+    OrderedDumper.add_representer(OrderedDict, _dict_representer)
+    return yaml.dump(data, stream, OrderedDumper, **kwds)
+
+
+
+def ind_recurse(pi_list, p_index=0):
+    '''Function that creates a list of list of all possible parameter indices'''
+    l = []  # return list
+    # Cycle through possible value indices for parameter with param index,
+    # p_index
+    for i in range(pi_list[p_index]):
+        if p_index == len(pi_list) - 1:
+            # If at the last index (end of recursion) start constructing index
+            # list
+            l += [[i]]
+        else:
+            # Prepend i value to front of sublists using recursion
+            lst = ind_recurse(pi_list, p_index + 1)
+            for x in lst:
+                l += [[i] + x]
+    return l
+
+
+
+def find_str_values(obj, pattern=r'^ChiParam\(.*\)'):
+    '''Recursive function to find ChiParams in program and returns a list of 
+    references to those objects.
+    '''
+    # Look through list with the index being the key of the object
+    if isinstance(obj, list):
+        for k, v in enumerate(obj):
+            # If you find a match to pattern, create and yeild object reference.
+            if re.match(pattern, str(v)):
+                yield ObjRef(obj, k)
+            # If another list or dictionary is encountered, recurse into it. Yeild any patterns you find
+            elif isinstance(v, (dict, list)):
+                for result in find_str_values(v, pattern):
+                    yield result
+    # Look through dictionary with the key being the key to the object reference
+    elif isinstance(obj, dict):
+        for k, v in obj.items():
+            if re.match(pattern, str(v)):
+                yield ObjRef(obj, k)
+            elif isinstance(v, (dict, list)):
+                for result in find_str_values(v, pattern):
+                    yield result
+    else:
+        return
+
+
+class ObjRef(object):
+    ''' A 'reference' to an object. This allows you to make changes to strings
+    or other objects in place of a dictionary or list like structure.
+    '''
+    def __init__(self, obj, key):
+        self.obj = obj
+        self.key = key
+
+    def Set(self, value):
+        self.obj[self.key] = value
+
+    def GetValue(self):
+        return self.obj[self.key]
+
+    def __repr__(self):
+        return self.obj[self.key]
+
+
+# TODO turn into node dir. Might not need this any more.
+def find_seed_dirs(path):
+    is_seed = re.compile(r's\d+$')
+    for current, dirnames, filenames in os.walk(path):
+        if is_seed.search(current):
+            yield os.path.abspath(current)
+
+
+def touch(fname, times=None):
+    """ Replicates the UNIX touch command """
+    with open(fname, 'a'):
+        os.utime(fname, times)
+
+
+def find_dirs(path):
+    """ Find all the child directories one level deep and return a list
+        of the absolute paths.
+    """
+    return [os.path.abspath(x) for x in (next(os.walk(path))[1])]
+
+
+##########################################
+if __name__ == "__main__":
+    print("Not implemented. This is strictly a library.")
