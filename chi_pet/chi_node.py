65b4b5027d2158c2d14c45b3e78221bc859318d0
Adam Lamson <adam.lamson@colorado.edu>
2019-05-31 20:45:30 -0600

    Prototyping chi_node and chi_param

---
+++ chi_pet/chi_node.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# In case of poor (Sh***y) commenting contact adam.lamson@colorado.edu
+# Basic
+import sys
+import os
+import yaml
+from shutil import rmtree
+from pathlib import Path
+from chi_param import ChiParam
+# import line_profiler
+# from math import *
+# Speed
+# from numba import jit
+
+
+"""@package docstring
+File: chi_node.py
+Author: Adam Lamson
+Email: adam.lamson@colorado.edu
+Description:
+"""
+
+
+class ChiNode(object):
+    def __init__(self, path, chiparams, yml_file_dict, params={}, level=0):
+        if isinstance(path, Path):
+            self._path = path
+        elif isinstance(path, str):
+            self._path = Path(path)
+        else:
+            raise TypeError(
+                " Path {} was neither pathlib object nor string".format(path))
+
+        self._chiparams = chiparams
+        self._yml_file_dict = yml_file_dict
+        self._params = params
+        self._level = level
+
+    def MakeSubnodes(self, subnode_dir="subnodes"):
+        snode_dir = self._path / subnode_dir
+        self.CreateDir(snode_dir)
+
+        for cp in self._chiparams:
+            # TODO tracer round coding
+            snode_path = snode_dir / cp._name
+            cnode = ChiNode(
+                snode_path,
+                cp,
+                self._yml_file_dict,
+                cp._name,
+                self._level - 1)
+            cnode.MakeNodeDirectory()
+
+    def MakeYamlFiles(self):
+        pass
+
+    def MakeNodeDirectory(self, overwrite=False):
+        node_created = self.CreateDir(self._path, overwrite)
+        self.MakeDataDirectory()
+        self.MakeYamlFiles()
+        if self._level < 0:
+            return
+        else:
+            self.MakeSubnodes()
+
+    def MakeDataDirectory(self):
+        self._data_dir = self._path / "data"
+        self.CreateDir(self._data_dir, overwrite=True)
+
+    def DumpData(self):
+        pass
+
+    def CreateDir(self, path, overwrite=False):
+        """!Create directory. If it exists it will be either overwritten or left
+        alone depending on the overwrite flag
+
+        @param path: pathlib Path to create
+        @return: Bool of whether a new directory was created or not
+
+        """
+        if path.exists():
+            if not overwrite:
+                print("{} exists and is being left alone.".format(path))
+                return False
+            else:
+                print("Removing {}".format(path))
+                rmtree(path)
+        else:
+            path.mkdir()
+
+
+##########################################
+if __name__ == "__main__":
+    cparams = [ChiParam("s" + str(i)) for i in range(10)]
+    yml_dict = {}
+    cnode = ChiNode(Path(os.getcwd()), cparams, yml_dict)
+    cnode.MakeNodeDirectory()
---
+++ chi_pet/chi_param.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# In case of poor (Sh***y) commenting contact adam.lamson@colorado.edu
+# Basic
+import sys
+import os
+# Testing
+# import pdb
+# import time, timeit
+# import line_profiler
+# Analysis
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib as mpl
+# import h5py
+# import yaml
+# from math import *
+# Speed
+# from numba import jit
+# Other importing
+
+
+"""@package docstring
+File: chi_param
+Author: Adam Lamson
+Email: adam.lamson@colorado.edu
+Description:
+"""
+
+
+class ChiParam(object):
+
+    """!Class that holds all the values of a changing parameter."""
+
+    def __init__(self, name, vals=[], level=0):
+        """!TODO: to be defined1.
+
+        @param name: TODO
+        @param vals: TODO
+
+        """
+        self._name = name
+        self._vals = vals
+        self._level = level
+
+
+class ChiParamVariations(object):
+
+    """!Docstring for ChiParamVariations. """
+
+    def __init__(self, chi_param_list=[]):
+        """!Initialize a list of varied parameters to make chi_nodes
+
+        @param chi_param_list: TODO
+
+        """
+        self._chi_param_list = chi_param_list
+
+
+##########################################
+if __name__ == "__main__":
+    print("Not implemented yet")
