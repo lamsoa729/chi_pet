3ce46833d4f0bb18609ca4a4b58df32bd55cf9ec
Adam Lamson <lamsoa729@gmail.com>
2023-11-30 11:57:54 -0500

    Made multilevel simulation

--- tests/test_chi_node.py
+++ tests/test_chi_node.py
@@ -65,11 +65,55 @@ def test_chi_node_multilevel_subnode_creation(mock_yaml_dict, mock_create_opts):
     # Add another ChiParam at a lower level
     yaml_dict = deepcopy(mock_yaml_dict)
     yaml_dict[MOCK_PARAM_DICT_PATH][
-        'chi_param_2'] = "ChiParam(name='pB', format_str='pB{:f}', values=[.1,.2,.3], level=1)"
+        'chi_param_2'] = "ChiParam(name='pB', format_str='pB{:.1f}', values=[.1,.2,.3], level=1)"
     chi_dict = ChiDict(param_dict=yaml_dict)
     cnode = ChiNode(root_path, chi_dict, opts=mock_create_opts)
     cnode.make_node_dir(root_path)
-    # mock_create_opts.param_file_paths = list(root_path.glob('*.yaml'))
     cnode.make_subnodes()
-    # TODO NEXT make this with chi_dict initialization
-    pass
+    for pa in [10, 20, 30]:
+        pa_dir_path = Path(f'tests/mock_root/subnodes/pA{pa}')
+        assert pa_dir_path.exists()
+
+        pa_subnode_path = pa_dir_path / 'subnodes'
+        assert pa_subnode_path.exists()
+
+        for pb in [.1, .2, .3]:
+            pb_dir_path = pa_subnode_path / f'pB{pb}'
+            assert pb_dir_path.exists()
+            assert (pb_dir_path / MOCK_PARAM_DICT_PATH).exists()
+            assert (pb_dir_path / MOCK_CHI_PARAM_DICT_PATH).exists()
+
+            assert (yaml.safe_load(
+                (pb_dir_path / MOCK_CHI_PARAM_DICT_PATH).open('r'))['var_param'] == pa)
+            assert (yaml.safe_load(
+                (pb_dir_path / MOCK_PARAM_DICT_PATH).open('r'))['chi_param_2'] == pb)
+
+
+def test_chi_node_combinatorics_subnode_creation(mock_yaml_dict, mock_create_opts):
+    root_path = Path.cwd() / 'tests/mock_root'
+    # Add another ChiParam at a lower level
+    yaml_dict = deepcopy(mock_yaml_dict)
+    yaml_dict[MOCK_PARAM_DICT_PATH][
+        'chi_param_2'] = "ChiParam(name='pB', format_str='pB{:.1f}', values=[.1,.2,.3])"
+    chi_dict = ChiDict(param_dict=yaml_dict)
+    cnode = ChiNode(root_path, chi_dict, opts=mock_create_opts)
+    cnode.make_node_dir(root_path)
+    cnode.make_subnodes()
+    # TODO NEXT finish this test
+    # for pa in [10, 20, 30]:
+    #     for pb in [.1, .2, .3]:
+    #     papb_dir_path = Path(f'tests/mock_root/subnodes/pA{pa}')
+    #     assert pa_dir_path.exists()
+
+    #     pa_subnode_path = pa_dir_path / 'subnodes'
+    #     assert pa_subnode_path.exists()
+
+    #         pb_dir_path = pa_subnode_path / f'pB{pb}'
+    #         assert pb_dir_path.exists()
+    #         assert (pb_dir_path / MOCK_PARAM_DICT_PATH).exists()
+    #         assert (pb_dir_path / MOCK_CHI_PARAM_DICT_PATH).exists()
+
+    #         assert (yaml.safe_load(
+    #             (pb_dir_path / MOCK_CHI_PARAM_DICT_PATH).open('r'))['var_param'] == pa)
+    #         assert (yaml.safe_load(
+    #             (pb_dir_path / MOCK_PARAM_DICT_PATH).open('r'))['chi_param_2'] == pb)
